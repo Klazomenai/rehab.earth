0d8b658409e5f05ed271140394dbf02c3b92461c
Tudor Georgescu <T.Georgescu@kainos.com>
2017-01-23 19:32:32 +0000

    Sorted out spacing

--- mailcow.tf
+++ mailcow.tf
@@ -2,23 +2,23 @@
 # https://mailcow.email/dockerized/
 
 resource "digitalocean_droplet" "mailcow" {
-    image = "docker-16-04"
-    name = "mailcow"
-    region = "${var.region}"
-    size = "2gb"
-    private_networking = true
-    ssh_keys = [
-      "${var.ssh_fingerprint}"
-    ]
+  image = "docker-16-04"
+  name = "mailcow"
+  region = "${var.region}"
+  size = "2gb"
+  private_networking = true
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
 
-connection {
+  connection {
     user = "root"
     type = "ssh"
     private_key = "${file("${var.pvt_key}")}"
     timeout = "2m"
-}
+  }
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
       "git clone https://github.com/Klazomenai/mailcow-dockerized",
       "cd ~/mailcow-dockerized && export MAILCOW_HOSTNAME=mail.rehab.earth; export TZ=\"Europe/London\"; ./generate_config.sh",
