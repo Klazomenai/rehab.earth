c2c72e8f62a1adcbf55a6fc371f2c7c41998277c
Tudor Georgescu <T.Georgescu@kainos.com>
2017-01-24 19:26:46 +0000

    Added mailcow droplet behind firewall (badly), and ensured the connection used the bastion jump credentials.

--- mailcow.tf
+++ mailcow.tf
@@ -17,10 +17,15 @@ resource "digitalocean_droplet" "mailcow" {
     type = "ssh"
     private_key = "${file("${var.pvt_key}")}"
     timeout = "2m"
+    bastion_host = "${digitalocean_droplet.firewall.ipv4_address}"
+    bastion_user = "${var.freebsd_user}"
   }
 
   provisioner "remote-exec" {
     inline = [
+      "ifconfig eth0 down",
+      # This is volatile and wrong and should go soon! See Issue #1 in GitHub
+      "sed -i '9,22d' /etc/network/interfaces.d/50-cloud-init.cfg",
       "git clone https://github.com/andryyy/mailcow-dockerized",
       "cd ~/mailcow-dockerized && export MAILCOW_HOSTNAME=mail.rehab.earth; export TZ=\"Europe/London\"; ./generate_config.sh",
       "cd ~/mailcow-dockerized/ && docker-compose up -d"
