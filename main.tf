a0790b07ff35d446209195344490a3d3858fe361
Tudor Georgescu <T.Georgescu@kainos.com>
2017-02-10 22:02:36 +0000

    Chef not able to pick up depends, trying berks

--- main.tf
+++ main.tf
@@ -28,6 +28,17 @@ resource "digitalocean_droplet" "mail" {
   # Set up chef. No chef-client --local provisioner yet
   provisioner "remote-exec" {
     inline = [
+      # Because 'activesupport requires Ruby version >= 2.2.2.'
+      "yum -y install gcc-c++ patch readline readline-devel zlib zlib-devel",
+      "yum -y install libyaml-devel libffi-devel openssl-devel make",
+      "yum -y install bzip2 autoconf automake libtool bison iconv-devel sqlite-devel",
+      "curl -sSL https://rvm.io/mpapis.asc | gpg --import -",
+      "curl -L get.rvm.io | bash -s stable",
+      ". /etc/profile.d/rvm.sh",
+      "rvm reload",
+      "rvm requirements run",
+      "rvm install 2.2.4",
+      "rvm use 2.2.4 --default",
       # Chef + Dependencies
       "curl -L https://omnitruck.chef.io/install.sh | sudo bash",
       "yum install -y git",
