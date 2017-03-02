d81fe014f4b335710955685fbd899c3653f4f861
Tudor Georgescu <T.Georgescu@kainos.com>
2017-03-02 20:08:39 +0000

    Typo in curl

--- terraform/bootstrap/scripts/all_the_things.sh
+++ terraform/bootstrap/scripts/all_the_things.sh
@@ -23,7 +23,7 @@ popd
 yum install -y docker
 systemctl start docker
 sudo systemctl enable docker
-curl -L \"https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 #
