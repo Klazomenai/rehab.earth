425d52079c351649f7a484feec3dc06b719280da
Tudor Georgescu <T.Georgescu@kainos.com>
2017-03-07 20:59:04 +0000

    Basic Vault to Consul connection

--- docker/vault/README.md
+++ docker/vault/README.md
@@ -1,6 +1,8 @@
 WARNING: Vault running with -dev!
 
+Spinning up individual containers for dev:
+
 ```sh
 docker-compose build
-docker-compose up -d
+docker-compose up
 ```
--- terraform/bootstrap/scripts/all_the_things.sh
+++ terraform/bootstrap/scripts/all_the_things.sh
@@ -25,6 +25,7 @@ systemctl start docker
 sudo systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+docker network create rehab.earth
 
 #
 # Vault
