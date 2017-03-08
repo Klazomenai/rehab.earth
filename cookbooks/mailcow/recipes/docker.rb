b3f887caba16676d0ef8843e4b9819fbf6be9e6e
Tudor Georgescu <T.Georgescu@kainos.com>
2017-03-08 12:33:09 +0000

    Removing docker-engine installation from mailcow recipe

--- cookbooks/mailcow/recipes/docker.rb
+++ cookbooks/mailcow/recipes/docker.rb
@@ -2,10 +2,6 @@ docker_installation 'default' do
   action :create
 end
 
-docker_installation_package 'default' do
-  action :create
-end
-
 docker_service 'default' do
   action [:create, :start]
 end
