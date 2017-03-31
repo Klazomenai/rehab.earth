24696a6491cf6422ade6846282b30176300a9a96
Tudor Georgescu <T.Georgescu@kainos.com>
2017-03-31 16:11:49 +0100

    Increasing droplet size

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
 variable "region" { default = "fra1" }
-variable "size" { default = "2gb" }
+variable "size" { default = "4gb" }
 variable "project" {}
 variable "tz" {}
 variable "base_snapshot_id" {}
