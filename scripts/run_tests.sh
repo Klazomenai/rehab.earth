975e3d9667fe7d84983fe878519c48c453612253
Tudor Georgescu <T.Georgescu@kainos.com>
2017-04-04 16:39:26 +0100

    Extending the pipeline to also configure where builds run

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,11 +2,13 @@
 
 set -e -o errexit
 
+echo "Hostname:" `hostname`
+
 pushd cookbooks/mailcow
 bundle install
 
 # Adding temporary debug diagnostics.
 kitchen diagnose --no-instances --loader
 
-bundle exec kitchen test
+#bundle exec kitchen test
 popd
